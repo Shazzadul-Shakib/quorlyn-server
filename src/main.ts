eda39850c826b67d0c1b8bceeb247c2557753456
Shazzadul-Shakib <shakib1186@gmail.com>
2026-07-30 21:19:18 +0600

    added auth module files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import type { Request, Response } from 'express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.getHttpAdapter().get('/', (_req: Request, res: Response) => {
+    res.send('Quorlyn server is running');
+  });
+  await app.listen(process.env.PORT ?? 5000);
+  console.log(`Server is running on port ${process.env.PORT ?? 5000}`);
 }
 bootstrap();
